701efbdd6bac40ea2964621909479c600843c5ff
Yinfeng Lin <yinfeng.lin@outlook.com>
2017-09-15 20:18:17 +0800

    add common.h

---
+++ common.h
@@ -0,0 +1,16 @@
+/* Define common properties of the game
+ * Include only when needed
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <QPoint>
+
+/* common properties of klotski game */
+extern const int kHorizontalUnit;
+extern const int kVerticalUnit;
+extern const int kWinPieceIndex;
+extern const QPoint kWinPosition;
+
+#endif
